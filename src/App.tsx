240e01b9140c87ec8ac15970ef80f9b8d218ddfe
Fang Jin <windmaomao@gmail.com>
2023-03-13 11:37:37 -0400

    add a modal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Pokemon from 'src/features/pokemon/Pokemon';
 import { ThemeProvider } from 'src/features/theme';
 import ThemeSelect from 'src/components/ThemeSelect';
+import Modal from 'src/components/Modal';
 import styles from './App.module.css';
 
 function App() {
   return (
     <ThemeProvider>
       <div className={styles.app}>
+        <Modal />
         <ThemeSelect />
         <div className={styles.title}>POKEDEX</div>
         <Pokemon />
